d5270dce055f526e4d5a5e32cfe66ac4d9bea120
BenOkojie <83321573+BenOkojie@users.noreply.github.com>
2025-10-02 13:02:45 -0400

    Fixed card and info for Benjamin Okojie

---
+++ src/team/benjamin.ts
@@ -0,0 +1,14 @@
+import { TeamMember } from "../types/TeamMember";
+
+export const benjamin: TeamMember = {
+  name: "Benjamin Okojie",
+  role: "Software Engineer",
+  funFact: "I run and bike a lot",
+  githubUsername: "benokojie",
+  // Feel free to customize your card's styles!
+  cardStyle: {
+    background: "bg-green-500",
+    border: "border border-black",
+    text: "text-yellow-500",
+  },
+};
